f52b2a0b376e8eb88c24d88500a4fd1b0d4ca16b
Hajdu Csaba <kyberszittya@protonmail.ch>
2026-03-18 20:51:27 +0100

    Adding CI/CD

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://hakiko.github.io/sze-it-mosze.github.io",
+  site: "https://sze-it-mosze.github.io",
+  base: "/sze-it-mosze.github.io",
 });
